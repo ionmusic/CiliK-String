b03b89f70c836aa488ce972d5a134631a92735c6
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-12-04 01:06:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import env
 import logging
+from dotenv import load_dotenv
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
